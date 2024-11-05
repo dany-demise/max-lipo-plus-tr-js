677d440a2bca77b45be29936fde751033bb6bc79
Anthony Gagnon <59715170+dany-demise@users.noreply.github.com>
2024-11-04 21:45:27 -0500

    Update max-lipo-tr-plus.js

--- max-lipo-tr-plus.js
+++ max-lipo-tr-plus.js
@@ -18,7 +18,6 @@ export async function maxLipoTrPlus(theFunction,
 
     if (!Module) {
         Module = await createMaxLipoTrPlusModule();
-        console.log("WebAssembly module loaded successfully.");
     }
 
     // Create an instance of JsFunction with the Rosenbrock function
@@ -26,10 +25,6 @@ export async function maxLipoTrPlus(theFunction,
 
     // Call the WebAssembly function
     const result = Module.max_lipo_tr_plus(jsFunc, lower_bounds, upper_bounds, max_calls);
-
-    // Log and return the result
-    console.log(result.x);
-    console.log(result.y);
 
     // Return the result as an object
     return { x: result.x, y: result.y };
